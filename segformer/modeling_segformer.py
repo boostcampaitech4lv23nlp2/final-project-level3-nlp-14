1bc97b06149004a8fff0faebdbc5cd85ee309e9a
falconjune <107377774+falconjune@users.noreply.github.com>
2023-01-25 10:49:04 +0900

    Update modeling_segformer.py
    add LSRA

--- segformer/modeling_segformer.py
+++ segformer/modeling_segformer.py
@@ -154,68 +154,64 @@ class SegformerEfficientSelfAttention(nn.Module):
         super().__init__()
         self.hidden_size = hidden_size
         self.num_attention_heads = num_attention_heads
-
+        
         if self.hidden_size % self.num_attention_heads != 0:
             raise ValueError(
                 f"The hidden size ({self.hidden_size}) is not a multiple of the number of attention "
                 f"heads ({self.num_attention_heads})"
             )
-
+        
         self.attention_head_size = int(self.hidden_size / self.num_attention_heads)
         self.all_head_size = self.num_attention_heads * self.attention_head_size
 
         self.query = nn.Linear(self.hidden_size, self.all_head_size)
-        self.key = nn.Linear(self.hidden_size, self.all_head_size)
-        self.value = nn.Linear(self.hidden_size, self.all_head_size)
-
+        self.kv = nn.Linear(self.hidden_size, self.all_head_size * 2)
+        
         self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
 
+        self.linear = config.linear
         self.sr_ratio = sequence_reduction_ratio
-        if sequence_reduction_ratio > 1:
-            self.sr = nn.Conv2d(
-                hidden_size, hidden_size, kernel_size=sequence_reduction_ratio, stride=sequence_reduction_ratio
-            )
+        
+        if not self.linear:
+            if sequence_reduction_ratio > 1:
+                self.sr = nn.Conv2d(hidden_size, hidden_size, kernel_size=sequence_reduction_ratio, stride=sequence_reduction_ratio)
+                self.layer_norm = nn.LayerNorm(hidden_size)
+        else:
+            self.pool = nn.AdaptiveAvgPool2d(7)
+            self.sr = nn.Conv2d(hidden_size, hidden_size, kernel_size=1, stride=1)
             self.layer_norm = nn.LayerNorm(hidden_size)
-
+            self.act = nn.GELU()
+            
     def transpose_for_scores(self, hidden_states):
         new_shape = hidden_states.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
         hidden_states = hidden_states.view(new_shape)
         return hidden_states.permute(0, 2, 1, 3)
 
-    def forward(
-        self,
-        hidden_states,
-        height,
-        width,
-        output_attentions=False,
-    ):
+    def forward(self, hidden_states, height, width, output_attentions=False):
+        batch_size, seq_len, num_channels = hidden_states.shape
         query_layer = self.transpose_for_scores(self.query(hidden_states))
 
-        if self.sr_ratio > 1:
-            batch_size, seq_len, num_channels = hidden_states.shape
-            # Reshape to (batch_size, num_channels, height, width)
+        if not self.linear:
+            if self.sr_ratio > 1:
+                hidden_states = hidden_states.permute(0, 2, 1).reshape(batch_size, num_channels, height, width)
+                hidden_states = self.sr(hidden_states)
+                hidden_states = hidden_states.reshape(batch_size, num_channels, -1).permute(0, 2, 1)
+                hidden_states = self.layer_norm(hidden_states)
+        else:
             hidden_states = hidden_states.permute(0, 2, 1).reshape(batch_size, num_channels, height, width)
-            # Apply sequence reduction
-            hidden_states = self.sr(hidden_states)
-            # Reshape back to (batch_size, seq_len, num_channels)
-            hidden_states = hidden_states.reshape(batch_size, num_channels, -1).permute(0, 2, 1)
+            hidden_states = self.sr(self.pool(hidden_states)).reshape(batch_size, num_channels, -1).permute(0, 2, 1)
             hidden_states = self.layer_norm(hidden_states)
+            hidden_states = self.act(hidden_states)
+            
+        kv = self.kv(hidden_states).reshape(batch_size, -1, 2, self.num_attention_heads, num_channels // self.num_attention_heads).permute(2, 0, 3, 1, 4)
+        key_layer, value_layer = kv[0], kv[1]
 
-        key_layer = self.transpose_for_scores(self.key(hidden_states))
-        value_layer = self.transpose_for_scores(self.value(hidden_states))
-
-        # Take the dot product between "query" and "key" to get the raw attention scores.
         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-
         attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-
-        # Normalize the attention scores to probabilities.
+        
         attention_probs = nn.functional.softmax(attention_scores, dim=-1)
-
-        # This is actually dropping out entire tokens to attend to, which might
-        # seem a bit unusual, but is taken from the original Transformer paper.
         attention_probs = self.dropout(attention_probs)
-
+        
         context_layer = torch.matmul(attention_probs, value_layer)
 
         context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
@@ -290,7 +286,7 @@ class SegformerDWConv(nn.Module):
 
         return hidden_states
 
-
+    
 class SegformerMixFFN(nn.Module):
     def __init__(self, config, in_features, hidden_features=None, out_features=None):
         super().__init__()
@@ -330,6 +326,7 @@ class SegformerLayer(nn.Module):
         self.layer_norm_2 = nn.LayerNorm(hidden_size)
         mlp_hidden_size = int(hidden_size * mlp_ratio)
         self.mlp = SegformerMixFFN(config, in_features=hidden_size, hidden_features=mlp_hidden_size)
+        #self.mlp = DestMixFFN(config, in_features=hidden_size, hidden_features=mlp_hidden_size)
 
     def forward(self, hidden_states, height, width, output_attentions=False):
         self_attention_outputs = self.attention(
